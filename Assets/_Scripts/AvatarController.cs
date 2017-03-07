eb51e198a13973d58a313d2773bbf52d759c18e4
Fred Sauer <fredsa@google.com>
2017-03-07 13:03:35 -0800

    fix spawning

---
+++ Assets/_Scripts/AvatarController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AvatarController : NetworkBehaviour {
+
+    public override void OnStartLocalPlayer() {
+        // Look towards origin
+        transform.rotation = Quaternion.LookRotation(-transform.localPosition, transform.up);
+        Camera.main.transform.parent.localPosition = transform.localPosition;
+        Camera.main.transform.parent.localRotation = transform.localRotation;
+    }
+	
+}
